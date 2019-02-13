91194ccb983dc973eb35a716799bb8793e0872ca
Ranjeet Ray <ranjeet.ray@pyramidconsultinginc.com>
2019-02-13 16:02:15 +0530

    Add project files.

---
+++ IValueConvertor/CheckBoxCheckConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace IValueConvertor
+{
+    public class CheckBoxCheckConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().ToUpper() == "MARRIED")
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool married = System.Convert.ToBoolean(value);
+            if (married == true)
+                return "Married";
+            else
+                return "Unmarried";
+        }
+    }
+}
---
+++ IValueConvertor/DataConversion.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IValueConvertor
+{
+    /// <summary>
+    /// Interaction logic for DataConversion.xaml
+    /// </summary>
+    public partial class DataConversion : Window
+    {
+        public DataConversion()
+        {
+            InitializeComponent();
+        }
+    }
+    public class AddConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType,
+               object parameter, System.Globalization.CultureInfo culture)
+        {
+            int result =
+                Int32.Parse((string)values[0]) + Int32.Parse((string)values[1]);
+            return result.ToString();
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes,
+               object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+
+}
